0699d68575c075eb4e92a9bed47e5e8705b05acc
Yoo Hyun Jun <fresh0911@naver.com>
2022-06-14 01:11:11 +0900

    third commit

---
+++ app/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+//모듈
+const express = require("express");
+const app = express();
+
+// 앱 세팅
+app.set("views", "./src/views"); //views 파일에 저장할것
+app.set("view engine", "ejs" ); //view 엔진을 ejs라는 뷰엔진을 사용할것
+
+// 라우팅
+const home = require("./src/routes/home");
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
+
+
+module.exports = app;

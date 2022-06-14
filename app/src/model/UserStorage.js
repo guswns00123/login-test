a0bc8899820b2846e364e8bde3999a4125f0c26c
Yoo Hyun Jun <fresh0911@naver.com>
2022-06-15 02:39:53 +0900

    모델 M 만들기

---
+++ app/src/model/UserStorage.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+class UserStorage {
+    static #users = {
+        id : ["jun", "yoo"],
+        psword : ["123", "1234"],
+        name : ["유현준", "현준"],
+    };
+    static getUsers(...fields) {
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) =>{
+            if (users.hasOwnProperty(field)){
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+            
+        }, {});
+        
+        return newUsers;
+    }
+
+}
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,9 +1,7 @@
 "use strict";
 
-const users = {
-    id : ["jun", "yoo"],
-    psword : ["123", "1234"],
-};
+const UserStorage = require("../../model/UserStorage");
+
 
 const output = {
     home : (req,res) => {
@@ -20,19 +18,20 @@ const process = {
     login : (req,res) => {
         const id = req.body.id,
         psword = req.body.psword;
-
+        
+        const users = UserStorage.getUsers("id", "psword");
+        const response = {};
         if (users.id.includes(id) ){
             const idx = users.id.indexOf(id);
             if (users.psword[idx] === psword){
-                return res.json({
-                    success: true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
-        return res.json({
-            success:false,
-            msg:"false",
-        });
+
+        response.success = false;
+        response.msg = "로그인에 실패하셨습니다.";
+        return res.json(response);
     },
 };
 

cd9299637fa2016b5b6ecc6b52c3e58d1e749618
Yoo Hyun Jun <fresh0911@naver.com>
2022-06-15 20:07:57 +0900

    회원가입 기능 추가 구현

--- app/src/model/User.js
+++ app/src/model/User.js
@@ -7,17 +7,25 @@ class User {
         this.body = body;
     }
     login() {
-        const body = this.body;
-        const {id, psword} = UserStorage.getUserInfo(body.id); //유저가 입력한 id값을 storage method로 전달
+        const client = this.body;
+        const {id, psword} = UserStorage.getUserInfo(client.id); //유저가 입력한 id값을 storage method로 전달
         
         if (id){ //id가 존재하냐 안하냐
-            if (id === body.id && psword === body.psword) {
+            if (id === client.id && psword === client.psword) {
                 return {success : true};
             }
             return {success : false, msg: "비밀번호가 틀렸습니다"};
         }
         return { success : false, msg: "존재하지않는 아이디입니다"};
         
+    }
+
+    register() {
+        const client = this.body;
+        const response = UserStorage.save(client);
+        return response;
+
+
     }
 }
 
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,5 +7,7 @@ const ctrl = require("./home.ctrl");
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.get("/register", ctrl.output.register);
+
 router.post("/login", ctrl.process.login);
+router.post("/register", ctrl.process.register);
 module.exports = router; //이 js파일을 내보내주는 방법 app.js파일과 연결

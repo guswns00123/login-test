97f10521e82a1163639bc4b1aed79734b76b8824
Yoo Hyun Jun <fresh0911@naver.com>
2022-06-17 05:35:15 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/app.js
+++ app/app.js
@@ -6,7 +6,18 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const morgan = require('morgan');
+const fs = require("fs");
+
+
+
 dotenv.config();
+
+// 라우팅
+const home = require("./src/routes/home");
+
+
+
 // 앱 세팅
 app.set("views", "./src/views"); //views 파일에 저장할것
 app.set("view engine", "ejs" ); //view 엔진을 ejs라는 뷰엔진을 사용할것
@@ -16,10 +27,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true}));
 //URL을 통해 전달되는 데이터에 한글, 공백 등 과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 
-// 라우팅
-const home = require("./src/routes/home");
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
+
+
+
+
 
 
 module.exports = app;
